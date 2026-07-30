b706895add7f08bf23e2cc0c5d1da7179b526de5
Anshul Negi <56960100+Anshul1310@users.noreply.github.com>
2026-07-30 20:02:28 +0530

    Update RetrofitClient.kt

--- MyApplication3/app/src/main/java/com/example/myapplication/network/RetrofitClient.kt
+++ MyApplication3/app/src/main/java/com/example/myapplication/network/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://10.0.2.2:4000"
+    private const val BASE_URL = "http://13.235.73.214:4000"
 
     val api: ApiService by lazy {
         Retrofit.Builder()
